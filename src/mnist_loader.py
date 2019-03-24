b5808bd991021227862cdd0fffe1ad469e795706
lixali <48850628+lixali@users.noreply.github.com>
2019-03-24 06:06:36 -0400

    Update mnist_loader.py

--- src/mnist_loader.py
+++ src/mnist_loader.py
@@ -4,7 +4,7 @@ mnist_loader
 
 A library to load the MNIST image data.  For details of the data
 structures that are returned, see the doc strings for ``load_data``
-and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
+and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the 
 function usually called by our neural network code.
 """
 
